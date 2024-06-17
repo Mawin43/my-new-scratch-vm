2d43e960fb6bfe45c7f0fedbba497be8f2e0fda9
Mawin <mawin43.work@gmail.com>
2024-06-17 12:21:49 +0700

    new

---
+++ src/extensions/scratch3_myExtension/index.js
@@ -0,0 +1,64 @@
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+
+class Scratch3myExtension {
+    // constructor(runtime) {
+    //     this.runtime = runtime;
+    //     this.x = '';
+
+    //     window.addEventListener('message', (event) => {
+    //         console.log("event", event)
+    //         this.x = event?.data?.detail?.value ?? "";
+    //     }, false);
+    // }
+
+    // getInfo() {
+    //     return {
+    //         id: 'myextension',
+    //         name: 'My Extension',
+    //         blocks: [
+    //             {
+    // opcode: 'getX',
+    // blockType: BlockType.REPORTER,
+    // text: 'get x',
+    //             }
+    //         ]
+    //     };
+    // }
+
+    // getX() {
+    //     return this.x;
+    // }
+
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.x = '';
+        window.addEventListener('message', (event) => {
+            console.log("event", event)
+            this.x = event?.data?.detail?.value ?? "";
+        }, false);
+    }
+
+    getInfo() {
+        return {
+            id: 'myExtension',
+            name: 'My Extension',
+            blocks: [
+                {
+                    opcode: 'getX',
+                    blockType: BlockType.REPORTER,
+                    text: 'get x',
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    getX() {
+        log.myText(this.x);
+        return this.x;
+    }
+}
+
+module.exports = Scratch3myExtension;
